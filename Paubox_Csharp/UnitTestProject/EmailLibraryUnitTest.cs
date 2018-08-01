f92e53e17d9e4c7df45305424c917702101055dc
vighneshtrivedi2004 <vighneshtrivedi2004@gmail.com>
2018-08-01 19:34:19 +0530

    1. Added Test Cases for both APIs 2. Added Test Data csv file for Send Message method

--- Paubox_Csharp/UnitTestProject/EmailLibraryUnitTest.cs
+++ Paubox_Csharp/UnitTestProject/EmailLibraryUnitTest.cs
@@ -1,5 +1,6 @@
 ﻿using EmailLib;
 using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.IO;
 
@@ -8,35 +9,133 @@ namespace UnitTestProject
     [TestFixture]
     public class EmailLibraryUnitTest
     {
-        public class GetTestDt
+        public class TestData
         {
-            string CSVFileName = @"E:\Dropbox\GENESYSBUZZ\Paubox REST API project\SendMessage_TestData.xlsx";
-            public int input { get; set; }
-            public int output { get; set; }
-            //public List<Message> GetTestData()
-            //{
-            //    Message objTestData = new Message();
-            //    List<Message> objListTestData = new List<Message>();
-            //    using (var inputStream = new FileStream(CSVFileName, FileMode.Open, FileAccess.Read))
-            //        {
-            //            using (var streamReader = new StreamReader(inputStream))
-            //            {
-            //                string inputLine;
-            //                while ((inputLine = streamReader.ReadLine()) != null)
-            //                {
-            //                    var data = inputLine.Split(',');
-            //                    List<int> param = new List<int>();
-            //                    for (int i = 0; i < data.Length; i++)
-            //                    {
-            //                        param.Add(Convert.ToInt32(data[i]));
-            //                    }
+            static string CSVFileName = @"E:\Paubox REST API project\SendMessage_TestData.csv"; 
+            public static List<Message> GetTestDataForSuccess()
+            {
+                Message objTestData = null;
+                List<Message> objListTestData = new List<Message>();
+                using (var inputStream = new FileStream(CSVFileName, FileMode.Open, FileAccess.Read))
+                {
+                    using (var streamReader = new StreamReader(inputStream))
+                    {
+                        string inputLine;
+                        inputLine = streamReader.ReadLine();// For Reading Columns
+                        while ((inputLine = streamReader.ReadLine()) != null)
+                        {
+                            var data = inputLine.Split(',');
+                            if (data[13] != "SUCCESS")
+                                continue;
 
-            //                    yield return new[] { param.ToArray() };
-            //                }
-            //            }
-            //        }
-                
-            //}
+                            objTestData = new Message();
+                            Content content = new Content();
+                            Header header = new Header();
+                            Attachment attachment = new Attachment();
+                            List<Attachment> listAttachments = new List<Attachment>();
+
+                            objTestData.Recipients = new string[] { data[1] };
+                            objTestData.Bcc = new string[] { data[2] };
+
+                            header.Subject = data[3];
+                            header.From = data[4];
+                            header.ReplyTo = data[5];
+
+                            if (!string.IsNullOrWhiteSpace(data[6]))
+                                objTestData.AllowNonTLS = bool.Parse(data[6]);
+
+                            if (string.IsNullOrWhiteSpace(data[7]))
+                                content.PlainText = null;
+                            else
+                                content.PlainText = data[7];
+
+                            if (string.IsNullOrWhiteSpace(data[8]))
+                                content.HtmlText = null;
+                            else
+                                content.HtmlText = data[8];
+
+
+                            if (Convert.ToInt32(data[9]) > 0)
+                            {
+                                attachment.FileName = data[10];
+                                attachment.ContentType = data[11];
+                                attachment.Content = data[12];
+
+                                listAttachments.Add(attachment);
+                            }
+
+                            objTestData.Header = header;
+                            objTestData.Content = content;
+                            objTestData.Attachments = listAttachments;                            
+                            objListTestData.Add(objTestData);
+                        }
+                    }
+
+                }
+                return objListTestData;
+            }
+
+            public static List<Message> GetTestDataForFailure()
+            {
+                Message objTestData = null;
+                List<Message> objListTestData = new List<Message>();
+                using (var inputStream = new FileStream(CSVFileName, FileMode.Open, FileAccess.Read))
+                {
+                    using (var streamReader = new StreamReader(inputStream))
+                    {
+                        string inputLine;
+                        inputLine = streamReader.ReadLine();// For Reading Columns
+                        while ((inputLine = streamReader.ReadLine()) != null)
+                        {
+                            var data = inputLine.Split(',');
+                            if (data[13] != "ERROR")
+                                continue;
+
+                            objTestData = new Message();
+                            Content content = new Content();
+                            Header header = new Header();
+                            Attachment attachment = new Attachment();
+                            List<Attachment> listAttachments = new List<Attachment>();
+
+                            objTestData.Recipients = new string[] { data[1] };
+                            objTestData.Bcc = new string[] { data[2] };
+
+                            header.Subject = data[3];
+                            header.From = data[4];
+                            header.ReplyTo = data[5];
+
+                            if (!string.IsNullOrWhiteSpace(data[6]))
+                                objTestData.AllowNonTLS = bool.Parse(data[6]);
+
+                            if (string.IsNullOrWhiteSpace(data[7]))
+                                content.PlainText = null;
+                            else
+                                content.PlainText = data[7];
+
+                            if (string.IsNullOrWhiteSpace(data[8]))
+                                content.HtmlText = null;
+                            else
+                                content.HtmlText = data[8];
+
+
+                            if (Convert.ToInt32(data[9]) > 0)
+                            {
+                                attachment.FileName = data[10];
+                                attachment.ContentType = data[11];
+                                attachment.Content = data[12];
+
+                                listAttachments.Add(attachment);
+                            }
+
+                            objTestData.Header = header;
+                            objTestData.Content = content;
+                            objTestData.Attachments = listAttachments;
+                            objListTestData.Add(objTestData);
+                        }
+                    }
+                }
+                return objListTestData;
+            }
         }
 
         #region Unit Tests for Get Email Disposition Method
@@ -91,5 +190,48 @@ namespace UnitTestProject
         }
 
         #endregion Unit Tests for Get Email Disposition Method
+
+        #region Unit Tests for Send Message Method
+
+        [Test, TestCaseSource(typeof(TestData), "GetTestDataForSuccess")]
+        public void TestSendMessage_ReturnSuccess(Message testMsg)
+        {           
+            var actualResponse = EmailLibrary.SendMessage(testMsg);
+            if (actualResponse == null || actualResponse.Data == null || actualResponse.SourceTrackingId == null)
+            {
+                Assert.Fail();
+            }
+            else if (actualResponse.Errors != null && actualResponse.Errors.Count > 0)
+            {
+                Assert.Pass();
+            }
+
+
+        }
+
+        [Test, TestCaseSource(typeof(TestData), "GetTestDataForFailure")]
+        public void TestSendMessage_ReturnError(Message testMsg)
+        {
+            var actualResponse = EmailLibrary.SendMessage(testMsg);
+            if (actualResponse != null)
+            {
+                if (actualResponse.Errors != null && actualResponse.Errors.Count > 0)
+                {
+                    Assert.Pass();
+                }
+                else
+                {
+                    Assert.Fail();
+                }
+            }
+            else 
+            {
+                Assert.Fail();
+            }
+        }
+
+        #endregion Unit Tests for Send Message Method
     }
+
+
 }
