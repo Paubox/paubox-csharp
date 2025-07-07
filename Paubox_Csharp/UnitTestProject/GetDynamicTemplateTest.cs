2e0aaab95150e42fac8a1195fa44a130f79e9685
Bradley Marques <bradleyrcmarques@gmail.com>
2025-07-07 08:08:33 +0200

    [WIP] Placeholder tests

---
+++ Paubox_Csharp/UnitTestProject/GetDynamicTemplateTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+using Moq;
+using Paubox;
+using Newtonsoft.Json;
+
+[TestFixture]
+public class GetDynamicTemplateTest
+{
+    private Mock<IAPIHelper> _mockApiHelper;
+    private EmailLibrary _emailLibrary;
+
+    [SetUp]
+    public void Setup()
+    {
+        _mockApiHelper = new Mock<IAPIHelper>();
+        _emailLibrary = new EmailLibrary("testApiKey", "testApiUser", _mockApiHelper.Object);
+    }
+
+    [Test]
+    public void TodoImplementThisTest()
+    {
+    }
+}
