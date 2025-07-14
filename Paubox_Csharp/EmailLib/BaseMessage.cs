129bfaf593a30e3c7ed47229cf5ed5b40c76a277
Bradley Marques <bradleyrcmarques@gmail.com>
2025-07-14 08:35:44 +0200

    Refactoring into BaseMessage class

---
+++ Paubox_Csharp/EmailLib/BaseMessage.cs
@@ -0,0 +1,119 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
+
+namespace Paubox
+{
+    /// <summary>
+    /// Base class for all message types
+    /// </summary>
+    /// <remarks>
+    /// Inherited by:
+    ///     1. Message
+    ///     2. TemplatedMessage
+    /// </remarks>
+    public class BaseMessage
+    {
+        public string[] Recipients { get; set; }
+        public string[] Bcc { get; set; }
+        public string[] Cc { get; set; }
+        public Header Header { get; set; }
+        public bool AllowNonTLS { get; set; } = false;
+        public string ForceSecureNotification { get; set; }
+        public List<Attachment> Attachments { get; set; }
+
+        /// <summary>
+        /// Convert the message object to a JSON object
+        /// </summary>
+        /// <returns>JObject</returns>
+        public virtual JObject ToJson()
+        {
+            if (this.Header == null) {
+                throw new ArgumentNullException("Header cannot be null.");
+            }
+
+            JObject headerJSON = JObject.FromObject(new Dictionary<string, string>() {
+                { "subject" , this.Header.Subject},
+                { "from" , this.Header.From},
+                { "reply-to" , this.Header.ReplyTo}
+            });
+
+            if (this.Header.CustomHeaders != null && this.Header.CustomHeaders.Count > 0)
+            {
+                foreach (var header in this.Header.CustomHeaders)
+                {
+                    headerJSON.Add(header.Key, header.Value);
+                }
+            }
+
+            // If there are attachments, then prepare attachment array JSON
+            JArray attachmentJSONArray = null;
+            if (this.Attachments != null && this.Attachments.Count > 0)
+            {
+                attachmentJSONArray = new JArray();
+                foreach (var attachment in this.Attachments)
+                {
+                    JObject attachmentJSON = JObject.FromObject(new
+                    {
+                        fileName = attachment.FileName,
+                        contentType = attachment.ContentType,
+                        content = attachment.Content
+                    });
+                    attachmentJSONArray.Add(attachmentJSON);
+                }
+            }
+
+            JObject MessageJSON = JObject.FromObject(new
+            {
+                recipients = this.Recipients,
+                bcc = this.Bcc,
+                cc = this.Cc,
+                headers = headerJSON,
+                allowNonTLS = this.AllowNonTLS,
+                attachments = attachmentJSONArray
+            });
+
+            bool? forceSecureNotificationValue = ReturnValidForceSecureNotificationValue(this.ForceSecureNotification);
+            if (forceSecureNotificationValue != null) // Add forceSecureNotificationValue to Request, only if it is not null
+            {
+                MessageJSON.Add("forceSecureNotification", forceSecureNotificationValue);
+            }
+
+            return MessageJSON;
+        }
+
+        /// <summary>
+        /// Returns valid nullable bool ForceSecureNotification value
+        /// </summary>
+        /// <param name="forceSecureNotification"></param>
+        /// <returns></returns>
+        protected static bool? ReturnValidForceSecureNotificationValue(string forceSecureNotification)
+        {
+            string forceSecureNotificationValue = null;
+            if (string.IsNullOrWhiteSpace(forceSecureNotification))
+            {
+                return null;
+            }
+            else
+            {
+                forceSecureNotificationValue = forceSecureNotification.Trim().ToLower();
+                if (forceSecureNotificationValue.Equals("true"))
+                {
+                    return true;
+                }
+                else if (forceSecureNotificationValue.Equals("false"))
+                {
+                    return false;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
--- Paubox_Csharp/EmailLib/Message.cs
+++ Paubox_Csharp/EmailLib/Message.cs
@@ -8,43 +8,17 @@ using System.Linq;
 
 namespace Paubox
 {
-    public class Message
+    public class Message : BaseMessage
     {
-        public string[] Recipients { get; set; }
-        public string[] Bcc { get; set; }
-        public string[] Cc { get; set; }
-        public Header Header { get; set; }
-        public bool AllowNonTLS { get; set; } = false;
-        public string ForceSecureNotification { get; set; }
         public Content Content { get; set; }
-        public List<Attachment> Attachments { get; set; }
 
         /// <summary>
-        /// Convert the Message object to a JSON object
+        /// Convert the Message object to a JSON object, including the Content
         /// </summary>
         /// <returns>JObject</returns>
-        public JObject ToJson()
+        public override JObject ToJson()
         {
-            if (this.Header == null) {
-                throw new ArgumentNullException("Message Header cannot be null.");
-            }
-            if (this.Content == null) {
-                throw new ArgumentNullException("Message Content cannot be null.");
-            }
-
-            JObject headerJSON = JObject.FromObject(new Dictionary<string, string>() {
-                { "subject" , this.Header.Subject},
-                { "from" , this.Header.From},
-                { "reply-to" , this.Header.ReplyTo}
-            });
-
-            if (this.Header.CustomHeaders != null && this.Header.CustomHeaders.Count > 0)
-            {
-                foreach (var header in this.Header.CustomHeaders)
-                {
-                    headerJSON.Add(header.Key, header.Value);
-                }
-            }
+            JObject messageJSON = base.ToJson();
 
             // Converting the html text to a base 64 string
             string base64EncodedHtmlText = null;
@@ -60,71 +34,9 @@ namespace Paubox
                 { "text/html" , base64EncodedHtmlText}
             });
 
-            //If there are attachments, then prepare attachment array JSON
-            JArray attachmentJSONArray = null;
-            if (this.Attachments != null && this.Attachments.Count > 0)
-            {
-                attachmentJSONArray = new JArray();
-                foreach (var attachment in this.Attachments)
-                {
-                    JObject attachmentJSON = JObject.FromObject(new
-                    {
-                        fileName = attachment.FileName,
-                        contentType = attachment.ContentType,
-                        content = attachment.Content
-                    });
-                    attachmentJSONArray.Add(attachmentJSON);
-                }
-            }
+            messageJSON.Add("content", contentJSON);
 
-            JObject MessageJSON = JObject.FromObject(new
-            {
-                recipients = this.Recipients,
-                bcc = this.Bcc,
-                cc = this.Cc,
-                headers = headerJSON,
-                allowNonTLS = this.AllowNonTLS,
-                content = contentJSON,
-                attachments = attachmentJSONArray
-            });
-
-            bool? forceSecureNotificationValue = ReturnValidForceSecureNotificationValue(this.ForceSecureNotification);
-            if (forceSecureNotificationValue != null) // Add forceSecureNotificationValue to Request, only if it is not null
-            {
-                MessageJSON.Add("forceSecureNotification", forceSecureNotificationValue);
-            }
-
-            return MessageJSON;
-        }
-
-        /// <summary>
-        /// Returns valid nullable bool ForceSecureNotification value
-        /// </summary>
-        /// <param name="forceSecureNotification"></param>
-        /// <returns></returns>
-        private static bool? ReturnValidForceSecureNotificationValue(string forceSecureNotification)
-        {
-            string forceSecureNotificationValue = null;
-            if (string.IsNullOrWhiteSpace(forceSecureNotification))
-            {
-                return null;
-            }
-            else
-            {
-                forceSecureNotificationValue = forceSecureNotification.Trim().ToLower();
-                if (forceSecureNotificationValue.Equals("true"))
-                {
-                    return true;
-                }
-                else if (forceSecureNotificationValue.Equals("false"))
-                {
-                    return false;
-                }
-                else
-                {
-                    return null;
-                }
-            }
+            return messageJSON;
         }
     }
 }
